0ab3617d99f643e47f71af43e80d4f198ee61bbf
isabellapagnon <37419368+isabellapagnon@users.noreply.github.com>
2020-03-30 22:58:27 -0300

    Add files via upload

---
+++ Maven/src/main/java/com/Grupo_15/Motorista.java
@@ -0,0 +1,33 @@
+package com.Grupo_15;
+// import org.apache.commons.csv.CSVFormat;
+// import org.apache.commons.csv.CSVParser;
+// import org.apache.commons.csv.CSVRecord;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Motorista{
+String cpf;
+String nome;
+Veiculo veiculo;
+FormaPagamento formaPagamento;
+public enum FormaPagamento {DINHEIRO,CARTAO,TODAS;}
+
+public String getCpf() {
+    return cpf;
+}
+
+public String getNome() {
+    return nome;
+}
+
+public Veiculo getVeiculo() {
+    return veiculo;
+}
+
+public FormaPagamento getFormaPagamento() {
+    return formaPagamento;
+}
+
+} 
---
+++ Maven/src/main/java/com/Grupo_15/Veiculo.java
@@ -0,0 +1,42 @@
+package com.Grupo_15;
+// import org.apache.commons.csv.CSVFormat;
+// import org.apache.commons.csv.CSVParser;
+// import org.apache.commons.csv.CSVRecord;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.lang.*;
+
+public class Veiculo {
+
+    public enum CategoriaVeiculo { SIMPLES, NORMAL, LUXO };
+    String placa;
+    String marca;
+    String cor;
+    CategoriaVeiculo categoria;
+
+    public Veiculo(String placa, String marca, String cor, CategoriaVeiculo categoria) {
+        this.placa = placa;
+        this.marca = marca;
+        this.cor = cor;
+        this.categoria = categoria;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public CategoriaVeiculo getCategoria() {
+        return categoria;
+    }
+
+}
